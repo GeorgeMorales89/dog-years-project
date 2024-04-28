15294a039656e6eb5cfef7ef138a3c9588e43f1c
George Morales <Moralesperezgeorge@gmail.com>
2024-04-27 18:36:34 -0700

    Create main.js

---
+++ main.js
@@ -0,0 +1,23 @@
+// Setting myAge to my current age
+const myAge = 34;
+
+// Setting earlyYears to 2
+let earlyYears = 2;
+
+// Multiplying earlyYears by 10.5
+earlyYears *= 10.5;
+
+// Setting laterYears to myAge - 2
+let laterYears = myAge - 2;
+
+// Multiplying laterYears by 4
+laterYears *= 4;
+
+// Adding earlyYears and laterYears to get my age in dog years
+let myAgeInDogYears = earlyYears + laterYears;
+
+// Setting myName to my name in lowercase
+let myName = "George".toLowerCase();
+
+// Printing my name, age, and age in dog years
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`)
